9a7bf3c508bef880fa1815319e734a962f7e6c24
Angelo Dias <oiangelodias@gmail.com>
2020-07-21 18:38:01 -0300

    finishing touches

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -46,7 +46,7 @@ class SendForgotPasswordEmailService {
         name: user.name,
         email: user.email,
       },
-      subject: '[GoBarber] Recuperação de senha',
+      subject: '[AppName] Recuperação de senha',
       templateData: {
         file: forgotPasswordTemplate,
         variables: {
